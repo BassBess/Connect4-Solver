087ee385defa5ad7fada451fb33420317c6722ff
BassBess <aliezzalrejal7@gmail.com>
2025-11-23 12:58:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "bot_hard.h"
 #include "defs.h"
 #include "board.h"
 #include "logic.h"
 #include "ai.h"
+#include "bot_hard.h" 
+
+int getIntInput() {
+    char input[20];
+    fgets(input, sizeof(input), stdin);
+    int value;
+    if (sscanf(input, "%d", &value) == 1) {
+        return value;
+    }
+    return -1; 
+}
 
 void setupPlayers(Player players[2], int *botMode, int *botDiff) {
-    printf("Welcome to Connect 4!!!!!!!!!!!!!!!!\n\n");
+    printf("Welcome to Connect 4!\n\n");
     printf("Choose game mode:\n");
     printf("1 - Player vs Player\n");
     printf("2 - Player vs Computer (Easy)\n");
@@ -17,9 +27,11 @@ void setupPlayers(Player players[2], int *botMode, int *botDiff) {
     printf("4 - Player vs Computer (Hard)\n");
     printf("Enter your choice (1-4): ");
     
-    int choice;
-    scanf("%d", &choice);
-    while (getchar() != '\n'); 
+    int choice = getIntInput();
+    while (choice < 1 || choice > 4) {
+        printf("Invalid choice. Try again: ");
+        choice = getIntInput();
+    }
     
     strcpy(players[0].name, "Player A");
     players[0].symbol = 'A';
@@ -34,7 +46,7 @@ void setupPlayers(Player players[2], int *botMode, int *botDiff) {
             *botDiff = 2;
             strcpy(players[1].name, "Computer (Medium)");
         } else {
-            *botDiff = 3; 
+            *botDiff = 3;
             strcpy(players[1].name, "Computer (Hard)");
         }
         
@@ -52,7 +64,7 @@ void setupPlayers(Player players[2], int *botMode, int *botDiff) {
 
 int main()
 {
-    init_hard_bot();
+    init_hard_bot(); 
     srand(time(NULL));
     int playAgain = 1;
     
@@ -66,20 +78,19 @@ int main()
 
         setupPlayers(players, &botMode, &botDiff);
 
-        int firstPlayer;
         printf("\nWho should go first?\n");
         printf("1 - %s (%c)\n", players[0].name, players[0].symbol);
         printf("2 - %s (%c)\n", players[1].name, players[1].symbol);
         printf("Enter choice (1 or 2): ");
         
-        scanf("%d", &firstPlayer);
-        while (getchar() != '\n');
-
-        if (firstPlayer == 2) {
-            current = 1; 
-        } else {
-            current = 0;
+        int first = getIntInput();
+        while (first != 1 && first != 2) {
+             printf("Invalid choice. Enter 1 or 2: ");
+             first = getIntInput();
         }
+
+        if (first == 2) current = 1;
+        else current = 0;
 
         printf("\nGame starting!\n");
         printBoard(board);
@@ -90,22 +101,20 @@ int main()
 
             if (botMode && current == 1)  
             {
-                printf("\nThinking...\n"); // Bitboard might take 1-2 seconds
+                printf("\nThinking...\n");
+                
                 column = getBotMove(board, botDiff, players[1].symbol, players[0].symbol);
                 
                 printf("%s (%c) chooses column %d\n", players[current].name, players[current].symbol, column);
             }
-        
+     
             else
             {
                 printf("\n%s (%c), choose a column (1-7): ", players[current].name, players[current].symbol);
+                column = getIntInput();
 
-                char input[10];
-                fgets(input, sizeof(input), stdin);
-
-                if (sscanf(input, "%d", &column) != 1)
-                {
-                    printf("Please enter a valid number (1-7).\n");
+                if (column == -1) {
+                    printf("Please enter a valid number.\n");
                     continue;
                 }
             }
@@ -124,12 +133,12 @@ int main()
 
             if (checkWin(board, row_placed, column - 1, players[current].symbol))
             {
-                printf("\n%s (%c) wins!\nPlay again? Press 'y' for YES, or any other key to quit: \n", players[current].name, players[current].symbol);
+                printf("\n%s (%c) wins!\n", players[current].name, players[current].symbol);
                 gameOver = 1;
             }
             else if (isBoardFull(board))
             {
-                printf("\nIt's a draw!\nPlay again? Press 'y' for YES, or any other key to quit: \n");
+                printf("\nIt's a draw!\n");
                 gameOver = 1;
             }
             else
@@ -138,6 +147,7 @@ int main()
             }
 
             if (gameOver) {
+                printf("Play again? (y/n): ");
                 char response[10];
                 fgets(response, sizeof(response), stdin);
                 
