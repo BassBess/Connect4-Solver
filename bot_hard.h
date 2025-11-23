49737f45be6bdd6a3d75dc238fc6f502645e6227
BassBess <aliezzalrejal7@gmail.com>
2025-11-23 23:04:42 +0200

    Update bot_hard.h

--- bot_hard.h
+++ bot_hard.h
@@ -1,6 +1,8 @@
 #ifndef BOT_HARD_H
 #define BOT_HARD_H
 
-void init_hard_bot(); 
-int getHardMove(char **b, char botSym); 
+void init_hard_bot(void);
+int getHardMove(char **b, char botSym);
+void cleanup_hard_bot(void);
+
 #endif
