823603d575091cc0efc864ff7599e125ca39683e
BassBess <aliezzalrejal7@gmail.com>
2025-11-23 11:26:07 +0200

    Update ai.c

--- ai.c
+++ ai.c
@@ -1,6 +1,7 @@
 #include "ai.h"
 #include "bot_easy.h"
 #include "bot_medium.h"
+#include "bot_hard.h" 
 
 int getBotMove(char **b, int difficulty, char botSym, char oppSym) {
     if (difficulty == 1) {
@@ -8,6 +9,10 @@ int getBotMove(char **b, int difficulty, char botSym, char oppSym) {
     } 
     else if (difficulty == 2) {
         return getMediumMove(b, botSym, oppSym);
+    }
+    else if (difficulty == 3) { 
+    
+        return getHardMove(b, botSym, oppSym);
     }
     
     return getEasyMove(b);
